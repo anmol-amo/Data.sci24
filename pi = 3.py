d5ecd591f68609dbf9b60e045abf900be2336535
anmol-amo <i_pearl55@yahoo.com>
2024-12-17 14:34:53 +0100

    update

---
+++ pi = 3.py
@@ -0,0 +1,23 @@
+pi = 3.14
+x = (int(pi))
+print(x)
+
+heltal = 5
+y = float(heltal)
+print(y)
+
+resultat = round (0.1 + 0.2, 1) #Avrunda till 1 decimal
+print (resultat)  #output 0.3
+
+tal1 = 0.1
+tal2 = 0.2
+tal1 = int(tal1 * 10)
+tal2 = int(tal2 * 10)
+print(tal1 + tal2)
+korrektsvar = (tal1 + tal2) / 10
+print(korrektsvar)
+
+x = "10"
+y = "23"
+summa = int(x) + int(y)
+print(f"summan är: {summa}")
--- testinghello.py
+++ testinghello.py
@@ -1,3 +1,4 @@
+'''''
 #print ("Hello, world")
 #print ("my name is Anmol")
 #print ("red,blue,green")
@@ -6,17 +7,71 @@
 #age = 32
 #print (f"vad heter du? {namn}, hur gammal är du? {age}")
 
+#x = 10
+#y = 20
+#z = (x + y)
+#print (f"summan av x och y är: {z}")
+
+#heltal = 10
+#flyttal = 3.14
+#text = "python är roligt"
+#sanningsvärde = True
+
+#print (type(heltal))
+#print (type(flyttal))
+#print (type(text))
+#print (type(sanningsvärde))
+
+
+#x = '123'
+#y = 2
+#z = int(x)
+#print ( z * y)
+
+#x = "123"
+#y = 2
+#z = int(x)*y
+#print(z)
+
+#a = 5 + 3
+#b = 5 // 2 # Heltalsdivision
+#c = 5 % 2 # Resten vid division
+#d = 5 ** 2 # 5 upphöjt till 2
+#print(a, b, c, d)
+'''
+
 x = 10
-y = 20
-z = (x + y)
-print (f"summan av x och y är: {z}")
+y = 3
 
-heltal = 10
-flyttal = 3.14
-text = "python är roligt"
-sanningsvärde = True
+print (x + y)
+print (x - y)
+print (x * y)
+print (x / y)
+print ( x // y)
+print (x % y)
+print (x ** y)
 
-print (type(heltal))
-print (type(flyttal))
-print (type(text))
-print (type(sanningsvärde))
+
+pi = 3.14
+x = (int(pi))
+print(x)
+
+heltal = 5
+y = float(heltal)
+print(y)
+
+resultat = round (0.1 + 0.2, 1) #Avrunda till 1 decimal
+print (resultat)  #output 0.3
+
+tal1 = 0.1
+tal2 = 0.2
+tal1 = int(tal1 * 10)
+tal2 = int(tal2 * 10)
+print(tal1 + tal2)
+korrektsvar = (tal1 + tal2) / 10
+print(korrektsvar)
+
+x = "10"
+y = "23"
+summa = int(x) + int(y)
+print(f"summan är: {summa}")
