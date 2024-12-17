af097b49e881da04a7f21c6955db7ba6733a526b
anmol-amo <i_pearl55@yahoo.com>
2024-12-17 10:20:01 +0100

    summan

--- testinghello.py
+++ testinghello.py
@@ -1,3 +1,12 @@
-print ("Hello, world")
-print ("my name is Anmol")
-print ("red,blue,green")
+#print ("Hello, world")
+#print ("my name is Anmol")
+#print ("red,blue,green")
+
+#namn = "Anmol"
+#age = 32
+#print (f"vad heter du? {namn}, hur gammal är du? {age}")
+
+x = 10
+y = 20
+z = (x + y)
+print (f"summan av x och y är: {z}")
