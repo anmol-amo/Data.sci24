f5c684afef8efa2c88a851aec9bc0f9aaf933834
anmol-amo <i_pearl55@yahoo.com>
2025-01-14 13:00:11 +0100

    practice

--- data.visualization/graph matplotlib.py
+++ data.visualization/graph matplotlib.py
@@ -3,6 +3,6 @@ import matplotlib.pyplot as plt
 labels = 'IT', 'Marketing', 'Data Science', 'Finance'
 values = [500, 156, 300, 510]
 explode = (0.05, 0.05, 0.05, 0.05) 
-
-plt.pie(values, labels=labels, autopct='%1.1f%%', shadow=False)
+color = ("pink", "blue", "yellow", "orange")
+plt.pie(values, labels=labels, autopct='%1.1f%%', shadow=False, colors=color)
 plt.show()
