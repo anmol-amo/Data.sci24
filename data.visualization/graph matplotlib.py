27344a155bda62b2bcfdb72217efbf5163c00d2a
anmol-amo <i_pearl55@yahoo.com>
2025-01-15 12:43:40 +0100

    öva

--- data.visualization/graph matplotlib.py
+++ data.visualization/graph matplotlib.py
@@ -7,7 +7,7 @@ explode = (0.05, 0.05, 0.05, 0.05)
 color = ("pink", "blue", "yellow", "orange")
 plt.pie(values, labels=labels, autopct='%1.1f%%', shadow=False, colors=color)
 plt.show()
-
+# to show pie charts
 """
 """""
 import matplotlib.pyplot as plt
--- data.visualization/matplot-exempel.py
+++ data.visualization/matplot-exempel.py
@@ -7,7 +7,7 @@ kurs = ["science",
 points = [25, 20, 30, 10,]
 
 plt.figure(figsize=(10, 8))
-plt.bar(kurs, points, color="red")
+plt.bar(kurs, points, color="skyblue")
 
 plt.xlabel("points", fontsize=12)
 plt.ylabel("kurs", fontsize=12)
