86e34ec02fa3136589f59795c36af65880b940e1
anmol-amo <i_pearl55@yahoo.com>
2025-01-21 10:21:51 +0100

    database

--- postgresql/import-db.py
+++ postgresql/import-db.py
@@ -4,30 +4,28 @@ import psycopg2
 # Anslut till PostgreSQL-databasen
 conn = psycopg2.connect(
     host="localhost",  # Byt ut med din värd
-    database="data_science",  # Byt ut med din databas
-    user="postgres",  # Byt ut med ditt användarnamn
-    password="postgres",  # Byt ut med ditt lösenord
+    database="postgres",  # Byt ut med din databas
+    user="postgres2",  # Byt ut med ditt användarnamn
+    password="postgres2",  # Byt ut med ditt lösenord
     port="5432"
     )
 cursor = conn.cursor()
 
-try:
+
     # Skapa tabellen i PostgreSQL om den inte redan finns
-    create_table_query = """
+create_table_query = """
     CREATE TABLE IF NOT EXISTS sales (
-        id SERIAL PRIMARY KEY,
-        date DATE,
-        product VARCHAR(50),
-        price NUMERIC,
-        quantity INTEGER,
-        sales NUMERIC
+    id SERIAL PRIMARY KEY,
+    date DATE,
+    product VARCHAR(50),
+    price NUMERIC,
+    quantity INTEGER,
+    sales NUMERIC
     );
     """
-    cursor.execute(create_table_query)
-    conn.commit()
-    print("Tabellen 'sales' har skapats i PostgreSQL!")
-except Exception as e:
-    print("Databas finns redan")
+cursor.execute(create_table_query)
+conn.commit()
+
 
 # Läs in CSV-filen
 
@@ -47,19 +45,19 @@ csv_file_path = "data.visualization/sales_data.csv"  # Sökvägen till din CSV-f
 df = pd.read_csv(csv_file_path)
 print(df.head())
 
-# # Iterera genom DataFrame och lägg till data i tabellen
-# for index, row in df.iterrows():
-#     insert_query = """
-#     INSERT INTO sales (date, product, price, quantity, sales)
-#     VALUES (%s, %s, %s, %s, %s)
-#     """
-#     cursor.execute(insert_query, (row['Date'], row['Product'], row['Price'], row['Quantity'], row['Sales']))
+ #Iterera genom DataFrame och lägg till data i tabellen
+for index, row in df.iterrows():
+    insert_query = """
+    INSERT INTO sales (date, product, price, quantity)
+    VALUES (%s, %s, %s, %s)
+    """
+    cursor.execute(insert_query, (row['Date'], row['Product'], row['Price'], row['Quantity']))
 
 # # Spara ändringar i databasen
-# conn.commit()
+conn.commit()
 
 # # Stäng anslutningen
-# cursor.close()
-# conn.close()
+cursor.close()
+conn.close()
 
-# print("CSV-filen har importerats till PostgreSQL!")
+print("CSV-filen har importerats till PostgreSQL!")
